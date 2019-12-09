444796c63807b3d253d5f94e55de6a7e7923f48c
jasg <jsgarcia@gmv.com>
2019-12-09 12:37:28 +0100

    Added exceptions to avoid the failure when the channel has no messages

--- telerocket.py
+++ telerocket.py
@@ -19,24 +19,33 @@ rocket = RocketChat(acronym, password, server_url=server_url)
 
 # Read last message in channel
 data = rocket.channels_history(channel_id, count=1).json()
-old_message = data['messages'][0]['msg']
+try:
+    old_message = data['messages'][0]['msg']
+except IndexError:
+    print("No messages in the channel yet \n")
+    old_message = None
 
 while True:
     data = rocket.channels_history(channel_id, count=1).json()
-    message = data['messages'][0]['msg']
-
-    # New message detected
-    if old_message != message:
-        # Send message to Telegram
-        name = username = data['messages'][0]['u']['name']
-        username = data['messages'][0]['u']['username']   
-        message_to_telegram = name + " (" + username + ")"+ ": " + message
-        print ("A new message to Telegram will be sent: \n" + message_to_telegram)
-        send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + message_to_telegram
-
-        response = requests.get(send_text)
+    try:
+        message = data['messages'][0]['msg']
     
-    old_message = message  
+        # New message detected
+        if old_message != message:
+            # Send message to Telegram
+            name = username = data['messages'][0]['u']['name']
+            username = data['messages'][0]['u']['username']   
+            message_to_telegram = name + " (" + username + ")"+ ": " + message
+            print ("A new message to Telegram will be sent: \n" + message_to_telegram)
+            send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + message_to_telegram
+
+            response = requests.get(send_text)
+        
+        old_message = message 
+
+    except IndexError:
+        print("No messages in the channel yet \n")
+        old_message = None 
 
     # Wait before take a look for new messages 
     time.sleep(1)
